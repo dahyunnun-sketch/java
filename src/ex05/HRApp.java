fde93f9840ff9b86e78379108b49cc2bd69b458e
dahyun <dahyunnun@gmail.com>
2026-08-07 13:21:36 +0900

    8/7

---
+++ src/ex05/HRApp.java
@@ -0,0 +1,84 @@
+package ex05;
+
+import java.util.List;
+
+import com.kh.util.inputUtil;
+
+import ex05.dao.DeptDao;
+import ex05.dao.EmpDao;
+import ex05.dto.DeptDTO;
+import ex05.dto.EmpDTO;
+
+public class HRApp {
+    // 필드로 만들기
+    // 사원목록 = empDao
+    static EmpDao empDao = new EmpDao();
+    // 부서목록
+    static DeptDao deptDao = new DeptDao();
+
+
+    public static void main(String[] args) {
+
+        System.out.println("""
+                ------------------------------------
+                사원관리 프로그램에 오신걸 환영합니다➰
+                ------------------------------------
+                """);
+        while (true) {
+            printMenu();
+            // util.get - 입력을 대기하고 있다가 사용자의 입력을 정수로 반환
+            int menu = inputUtil.getInt("메뉴를 입력해주세요");
+    
+            if(menu == 1){
+                List<EmpDTO> list = empDao.findAll();
+    
+                System.out.println(list);
+            } else if(menu == 2){
+                List<DeptDTO> list = deptDao.findAll();
+    
+                System.out.println(list);
+            } else if (menu == 0) {
+                System.out.println("""
+                    ---------------------------    
+                    사원관리 프로그램을 종료합니다
+                    ---------------------------
+                    """);
+                System.exit(0);
+            } else if (menu == 3) {
+                String name = inputUtil.getString("사원명 : ");
+                // 한명의 사원정보를 반환
+                EmpDTO empDto = empDao.find(name);
+                // null이 아니면 사원정보를 출력
+                if (empDto != null) {
+                    System.out.println(empDto);
+
+                    String ent_yn = inputUtil.getString("퇴사 처리 하시겠습니까 ?(Y/N)")
+                    if ("y".equalsIgnoreCase(ent_yn)) {
+                        // 퇴사 처리 - 사번을 이용해서 퇴사 처리
+                        int res = empDao.updateEntYn(empDto.getEmpId());
+                        // 0인 이유 = 1개 이상의 행이 영향을 받음
+                        if (res > 0) {
+                            
+                            System.out.println("퇴사 처리 되었습니다.");
+                        }
+                    }
+                } else {
+                    System.out.println("%s님은 존재하지 않습니다.".formatted(name));
+                }
+            }
+        }
+    }
+
+    // 정적메서드는 정적메서드만 호출가능
+    // 같은 패키지 내에서는 접제 생략가능
+    private static void printMenu(){
+        System.out.println("""
+                메뉴
+                1. 사원목록
+                2. 부서목록
+                3. 사원조회 - 이름
+                4. 사원조회 - 사번
+                0. 프로그램 종료
+                """);
+    }
+}
---
+++ src/ex05/dao/EmpDao.java
@@ -0,0 +1,115 @@
+package ex05.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import ex05.dto.EmpDTO;
+import ex05.util.DBUtil;
+
+// 데이터베이스에 접근해서 쿼리 질의결과를 반환
+// xxDao
+// Data Access Object의
+// 자바 애플리케이션에서 데이터베이스(DB)에 접근하여 데이터의 조회, 삽입, 수정, 삭제(CRUD) 
+// 작업을 전담하는 객체(클래스)를 의미
+public class EmpDao {
+
+    // 사원의 정보를 조회 하고 리스트를 반환
+    // xxxDto
+    // Data Transfer Object
+    // 데이터를 계층 간에 전달하기 위해 순수하게 데이터를 담아두는 바구니
+    public List<EmpDTO> findAll(){
+        List<EmpDTO> list = new ArrayList<>();
+
+        // 사원정보를 조회 하는 쿼리
+        // ORDER BY 정렬컬럼 DESC(내림차순)
+        String sql = "SELECT * FROM EMP ORDER BY SALARY DESC";
+
+        // 자원 반납 -> try () 안에서 생성하면 구문이 끝나면 자동으로 반납해줘요!!
+        try (
+            // 1. connection 연결
+            Connection con = DBUtil.getConnection();
+            // 2. 쿼리 질의 객체 생성
+            Statement stmt = con.createStatement();
+            // 3. 결과집합을 반환 -> 객체생성 -> 리스트에 담기
+            ResultSet rs = stmt.executeQuery(sql);
+        ) {
+
+            while (rs.next()) {
+                String empId = rs.getString(1);                
+                String empName = rs.getString(2);                
+                int salary = rs.getInt(8);                
+                
+                // dto생성및 리스트에 담기
+                list.add(new EmpDTO(empId, empName, salary));
+            }
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+        return list;
+    }
+
+
+
+    // 사원조회 - 사원반환, 사원이 없으면 - null
+    public EmpDTO find(String name) {
+        EmpDTO emp = null;
+        String sql = "SELECT * FROM EMP WHERE EMP_NAME = '%s'".formatted(name);
+        try (
+            // 1. connection 연결
+            Connection con = DBUtil.getConnection();
+            // 2. 쿼리 질의 객체 생성
+            Statement stmt = con.createStatement();
+            // 3. 결과집합을 반환 -> 객체생성 -> 리스트에 담기
+            ResultSet rs = stmt.executeQuery(sql);
+        ) {
+
+            if (rs.next()) {
+                String empId = rs.getString(1);                
+                String empName = rs.getString(2);                
+                int salary = rs.getInt(8);                
+                     
+                // 한명의 사원 정보를 반환
+                return new EmpDTO(empId, empName, salary);
+            }
+
+
+        } catch(Exception e){
+
+        }
+
+        return emp;
+    }
+
+
+
+   
+
+
+
+    public int updateEntYn(String empId) {
+       int res = 0;
+       String sql = "UPDATE EMP SET ENT_YN='Y' WHERE EMP_ID = '%s'".formatted(empId);
+       
+       try (// 1. connection 연결
+            Connection con = DBUtil.getConnection();
+            // 2. 쿼리 질의 객체 생성
+            Statement stmt = con.createStatement();
+            // 3. 결과집합을 반환 -> 객체생성 -> 리스트에 담기
+        )
+        {
+            res = stmt.executeUpdate(sql);
+        
+        } catch (Exception e) {
+            
+        }
+
+        return res;
+    }
+}
